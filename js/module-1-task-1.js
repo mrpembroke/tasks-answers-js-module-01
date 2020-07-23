c95b9499fefde471eedbbf8929ac5f46c8092638
Dmytro Petrenko <blinker182ca@gmail.com>
2020-07-23 12:28:16 +0300

    done with 1st task

---
+++ js/module-1-task-1.js
@@ -0,0 +1,17 @@
+// console.log(10);
+
+const name = '«Генератор защитного поля»'; // Write code in this line
+let price = 1000; // Write code in this line
+console.log('price = ' + price);
+const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code in this line
+console.log(firstMessage);
+
+price = 2000;
+console.log('price = ' + price);
+const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code in this line
+console.log(secondMessage);
+// console.log(firstMessage);
+// 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
+
+// console.log(secondMessage);
+// 'Выбран «Генератор защитного поля», цена за штуку 2000 кредитов'
